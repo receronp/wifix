2846b17acc83cebba394e740b9b14a78ba80b9be
Raul Ceron <receronp@gmail.com>
2020-12-10 10:56:36 +0100

    Fix absolute paths and shebang line

--- fix_wifi.py
+++ fix_wifi.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import json
 import requests
 
 data = {}
-with open("data.json") as input_data:
+with open("/home/pi/scripts/wifix/data.json") as input_data:
     data = json.load(input_data)
 
 response = requests.post(
@@ -15,8 +15,7 @@ response = requests.post(
         "ipaddr": data["ipaddr"],
         "action": data["action"],
     },
-    headers={"accept": "application/json"},
 )
 
-with open("response.html", "w") as output_file:
+with open("/home/pi/scripts/wifix/response.html", "w") as output_file:
     output_file.write(response.text)
