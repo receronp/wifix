6de6fc976939d769f21af6d9e543d48ada5e0bc5
Raul Ceron <receronp@gmail.com>
2020-12-12 11:28:45 +0100

    Fix to GET dynamic IP address and include it in the POST request.

--- fix_wifi.py
+++ fix_wifi.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
+import os
+import re
 import json
 import requests
 
 data = {}
-with open("/home/pi/scripts/wifix/data.json") as input_data:
+path = os.path.dirname(os.path.abspath(__file__))
+
+html = requests.get(url="https://login.rz.ruhr-uni-bochum.de/cgi-bin/start").text
+ip_rgx = re.search(r"(\d+.){3}\d+", html)
+
+with open(path + "/data.json") as input_data:
     data = json.load(input_data)
 
 response = requests.post(
     url="https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin",
     data={
-        "code": data["code"],
+        "code": "1",
         "loginid": data["loginid"],
         "password": data["password"],
-        "ipaddr": data["ipaddr"],
-        "action": data["action"],
+        "ipaddr": ip_rgx[0],
+        "action": "Login",
     },
 )
 
-with open("/home/pi/scripts/wifix/response.html", "w") as output_file:
+with open(path + "/response.html", "w") as output_file:
     output_file.write(response.text)
